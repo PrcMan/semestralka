27aa3b359620649452e84aad821810886151404b
Jakub Kramár <kramar.jakub@gmail.com>
2017-12-04 20:03:46 +0100

    Added ENUM for genres

---
+++ src/Genre.java
@@ -0,0 +1,16 @@
+/*
+ * Made for purposes connected with semestral work
+ * Copying and other editing without permission of the author
+ * is strictly prohibited and is punishable by law. This work is by 
+ * all means property of the author.
+ */
+
+/**
+ *
+ * @author krama
+ */
+public enum Genre {
+    ROCK, POP_PUNK, PUNK, ALTERNATIVE, METALCORE, POST_HARDCORE, METAL, HARDCORE,
+    DEATHCORE, PUNK_ROCK, HARD_ROCK, ELECTRONIC, RAP_ROCK, POP, VOICE_MEMO,RAP, 
+    ELECTRONIC_ROCK, OTHER, REMIX 
+}
