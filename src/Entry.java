75df66b91802b2235dd5229958b5bf66a0d4145c
Jakub Kramár <kramar.jakub@gmail.com>
2017-12-04 21:09:43 +0100

    Entry for one song

---
+++ src/Entry.java
@@ -0,0 +1,103 @@
+/*
+ * Made for purposes connected with semestral work
+ * Copying and other editing without permission of the author
+ * is strictly prohibited and is punishable by law. This work is by 
+ * all means property of the author.
+ */
+
+/**
+ *
+ * @author krama
+ */
+public class Entry {
+    private String aSong;
+    private String aAlbum;
+    private String aBand;
+    private int aYear;
+    private int aDuration;
+    private Genre aGenre;
+    /**
+     * 
+     * @param paSong
+     * @param paAlbum
+     * @param paBand
+     * @param paYear
+     * @param paDuration
+     * @param paGenre 
+     */
+    public Entry(String paSong, String paAlbum, String paBand, int paYear, int paDuration, Genre paGenre){
+        this.aSong = paSong;
+        this.aAlbum = paAlbum;
+        this.aBand = paBand;
+        this.aYear = paYear;
+        this.aDuration = paDuration;
+        this.aGenre = paGenre;
+    }
+    /**
+     * Getter for song
+     * @return song
+     */
+    public String getSong(){
+        return this.aSong;
+    }
+    /**
+     * 
+     * @param paSong 
+     */
+    public void setSong(String paSong){
+        this.aSong = paSong;
+    }
+    /**
+     * Getter for album
+     * @return album
+     */
+    public String getAlbum(){
+        return this.aAlbum;
+    }
+    /**
+     * 
+     * @param paAlbum 
+     */
+    public void setAlbum(String paAlbum){
+        this.aAlbum = paAlbum;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public String getBand(){
+        return this.aBand;
+    }
+    /**
+     * 
+     * @param paBand 
+     */
+    public void setBand(String paBand){
+        this.aBand = paBand;
+    }
+    
+    public int getYear(){
+        return this.aYear;
+    }
+    
+    public void setYear(int paYear){
+        this.aYear = paYear;
+    }
+    
+    public int getDuration(){
+        return this.aDuration;
+    }
+    
+    public void setDuration(int paDuration){
+        this.aDuration = paDuration;
+    }
+    
+    public Genre getGenre(){
+        return this.aGenre;
+    }
+    
+    public void setGenre(Genre paGenre){
+        this.aGenre = paGenre;
+    }
+    
+}
